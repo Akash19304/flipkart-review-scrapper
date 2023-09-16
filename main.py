ae6a5f1dcc6d282b77c46e25edac254651325855
Akash19304 <123761006+Akash19304@users.noreply.github.com>
2023-09-16 17:31:21 +0530

    changed from multiple products to single due to error

--- main.py
+++ main.py
@@ -26,39 +26,38 @@ def index():
             flipkart_html = bs(flipkart_page, "html.parser")
             bigboxes = flipkart_html.find_all("div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
-
+            box = bigboxes[0]
             reviews = []
-            for i in range(1,5):
-                product_name = bigboxes[i].find_all("div", {"class": "_4rR01T"})[0].text
-                productLink = "https://www.flipkart.com" + bigboxes[i].div.div.div.a['href']
-                prodRes = requests.get(productLink)
-                prodRes.encoding = 'utf-8'
-                prod_html = bs(prodRes.text, "html.parser")
+            product_name = box.find_all("div", {"class": "_4rR01T"})[0].text
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            prodRes = requests.get(productLink)
+            prodRes.encoding = 'utf-8'
+            prod_html = bs(prodRes.text, "html.parser")
 
-                comment_boxes = prod_html.find_all("div", {'class': "_16PBlm"})
+            comment_boxes = prod_html.find_all("div", {'class': "_16PBlm"})
 
 
-                for commentbox in comment_boxes:
-                    try:
-                        name = commentbox.div.div.find_all("p", {"class": "_2sc7ZR _2V5EHH"})[0].text
-                        rating = commentbox.div.div.div.div.text
-                        comment_header = commentbox.div.div.div.p.text
-                        comtag = commentbox.div.div.find_all('div', {'class': ''})
-                        comment = comtag[0].div.text
-                    except:
-                        name = "No Name"
-                        rating = "No Rating"
-                        comment_header = "No Header"
-                        comment = "No Review"
-                    try:
-                        my_dict = {"Product": product_name,
+            for commentbox in comment_boxes:
+                try:
+                    name = commentbox.div.div.find_all("p", {"class": "_2sc7ZR _2V5EHH"})[0].text
+                    rating = commentbox.div.div.div.div.text
+                    comment_header = commentbox.div.div.div.p.text
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    comment = comtag[0].div.text
+                except:
+                    name = "No Name"
+                    rating = "No Rating"
+                    comment_header = "No Header"
+                    comment = "No Review"
+                try:
+                    my_dict = {"Product": product_name,
                                 "Name": name,
                                 "Rating": rating,
                                 "Comment Header": comment_header,
                                 "Review": comment}
-                        reviews.append(my_dict)
-                    except Exception as e:
-                        print(e)
+                    reviews.append(my_dict)
+                except Exception as e:
+                    print(e)
 
             return render_template('results.html', reviews=reviews[0:(len(reviews) - 1)])
         except Exception as e:
